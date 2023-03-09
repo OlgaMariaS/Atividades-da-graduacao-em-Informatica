8a53541de162562dc904c13677b26795374afac0
Olga Maria dos Santos Gonçalves <olgamariasssantos@gmail.com>
2023-03-09 15:07:46 -0300

    criação deque.c

--- Estrutura-de-dados/Listas/deque.c
+++ Estrutura-de-dados/Listas/deque.c
@@ -1,104 +1,154 @@
 #include <stdio.h>
 #include <stdlib.h>
+/* Autor: Olga    
+março/2023 
+Objetivo: Fazer um deque
+-Inicializar;
+-verificar se esta vazio;
+-inserção pela direita e esquerda
+-remoção pela direita e esquerda
+-Exibir o deque
+    -da esquerda pra direita 
+    -da direita pra esquerda 
+-Encerrar */
 
-// Definição da estrutura do nó da lista
-typedef struct node {
-    int value;
-    struct node* prev;
-    struct node* next;
-} Node;
+/*____________________________________CRIAÇÃO DO DEQUE_________________________________*/
+struct tipo_item{
+    int chave;
+};
 
-// Definição da estrutura do deque
-typedef struct deque {
-    Node* front;
-    Node* rear;
-} Deque;
+struct tipo_celula{
+    struct tipo_item item;
+    struct tipo_celula *prox;
+    struct tipo_celula *ant;
+};
 
-// Função para criar um nó
-Node* createNode(int value) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->value = value;
-    newNode->prev = NULL;
-    newNode->next = NULL;
-    return newNode;
+struct tipo_deque{
+    struct tipo_celula *sentinela;
+};
+
+void inicializa(struct tipo_deque *deque){
+    deque->sentinela=(struct tipo_celula *)malloc(sizeof(struct tipo_celula));
+    deque->sentinela->prox=deque->sentinela;
+    deque->sentinela->ant=deque->sentinela;
+}
+/*_______________________________Verifica se está vazia_________________________________*/
+
+int vazia(struct tipo_deque *deque){
+    return deque->sentinela->prox == deque->sentinela;
+}
+/*______________________________________INSERÇÕES_______________________________________*/
+
+void insere_esquerda(struct tipo_deque *l, struct tipo_item x){
+    struct tipo_celula *novo;
+    novo=(struct tipo_celula *)malloc(sizeof(struct tipo_celula));
+    novo->item=x;
+    novo->prox = l->sentinela->prox;
+    novo->ant = l->sentinela;
+    l->sentinela->prox->ant = novo;
+    l->sentinela->prox = novo;
 }
 
-// Função para criar um deque vazio
-Deque* createDeque() {
-    Deque* newDeque = (Deque*)malloc(sizeof(Deque));
-    newDeque->front = NULL;
-    newDeque->rear = NULL;
-    return newDeque;
+void insere_direita(struct tipo_deque *l, struct tipo_item x){
+    struct tipo_celula *novo;
+    novo=(struct tipo_celula *)malloc(sizeof(struct tipo_celula));
+    novo->item=x;
+    novo->prox=l->sentinela;
+    novo->ant=l->sentinela->ant;
+    l->sentinela->ant->prox=novo;
+    l->sentinela->ant=novo;
 }
-
-// Função para verificar se o deque está vazio
-int isEmpty(Deque* deque) {
-    return deque->front == NULL;
-}
-
-// Função para inserir um item na frente do deque
-void insertFront(Deque* deque, int value) {
-    Node* newNode = createNode(value);
-    if (isEmpty(deque)) {
-        deque->front = newNode;
-        deque->rear = newNode;
-    } else {
-        newNode->next = deque->front;
-        deque->front->prev = newNode;
-        deque->front = newNode;
+/*______________________________________REMOÇÃO________________________________________*/
+int remove_esquerda(struct tipo_deque *l, struct tipo_item *x){
+    struct tipo_celula *del;
+    if(!vazia(l)){
+        del=l->sentinela->prox;
+        *x=del->item;
+        l->sentinela->prox = del->prox;
+        del->prox->ant = l->sentinela;
+        free(del);
+    }else{
+        printf("Deque vazio");
     }
 }
-
-// Função para inserir um item no final do deque
-void insertRear(Deque* deque, int value) {
-    Node* newNode = createNode(value);
-    if (isEmpty(deque)) {
-        deque->front = newNode;
-        deque->rear = newNode;
-    } else {
-        newNode->prev = deque->rear;
-        deque->rear->next = newNode;
-        deque->rear = newNode;
+int remove_direita(struct tipo_deque *l, struct tipo_item *x){
+    struct tipo_celula *del;
+    if(!vazia(l)){
+        del=l->sentinela->ant;
+        *x=del->item;
+        l->sentinela->ant = del->ant;
+        l->sentinela->ant->prox = l->sentinela;
+        free(del);
+    }else{
+        printf("Deque vazio");
     }
 }
-
-// Função para remover um item da frente do deque
-int deleteFront(Deque* deque) {
-    if (isEmpty(deque)) {
-        printf("Deque is empty\n");
-        return -1;
-    }
-    int value = deque->front->value;
-    Node* temp = deque->front;
-    deque->front = deque->front->next;
-    if (deque->front == NULL) {
-        deque->rear = NULL;
-    } else {
-        deque->front->prev = NULL;
-    }
-    free(temp);
-    return value;
+/*___________________________________Impressão ___________________________________________*/
+void impressaoEsqDir(struct tipo_deque *d){
+    struct tipo_celula *aux;
+    if(!vazia(d)){
+        aux = d->sentinela->prox;
+        while (aux != d->sentinela->ant->prox) {
+            printf(" %i ", d->sentinela->item.chave );
+            aux = aux->prox;
+        }
+    }else{
+        printf("Deque vazio");
+    } 
 }
-
-// Função para remover um item do final do deque
-int deleteRear(Deque* deque) {
-    if (isEmpty(deque)) {
-        printf("Deque is empty\n");
-        return -1;
-    }
-    int value = deque->rear->value;
-    Node* temp = deque->rear;
-    deque->rear = deque->rear->prev;
-    if (deque->rear == NULL) {
-        deque->front = NULL;
-    } else {
-        deque->rear->next = NULL;
-    }
-    free(temp);
-    return value;
+void impressaoDirEsq(struct tipo_deque *d){
+    struct tipo_celula *aux;
+    if(!vazia(d)){
+        aux = d->sentinela->ant;
+        while (aux != d->sentinela->prox->ant) {
+            printf(" %i ", d->sentinela->item.chave );
+            aux = aux->ant;
+        }
+    }else{
+        printf("Deque vazio");
+    } 
 }
+/*_________________________________________________________________________________________*/
+int main(){
+    /*Criação do deque */
+    struct tipo_deque *dequePrincipal;
+    inicializa(dequePrincipal); 
 
-// Função para imprimir o deque
-void printDeque(Deque* deque) {
-    Node* current = deque->front;
-    printf("Deque");
+    struct tipo_item elemento;
+
+    int menu;
+
+    printf("Algoritmo de Deque \n Escolha uma opcao \n  1 - Inserir a esquerda \n  2 - Inserir a direita \n  3 - Remover a esquerda \n  4 - Remover a direita \n  5 - Imprimir esq->dir \n  6 - Imprimir da dir->esq \n  7 - Encerrar \n");
+    scanf("%i", &menu);
+
+    if (menu == 1){
+        printf("Informe o elemento para inserir: ");
+        scanf("%i", &elemento.chave);
+
+        insere_direita(dequePrincipal, elemento);
+    }
+    if (menu == 2){
+        printf("Informe o elemento para inserir: ");
+        scanf("%i", &elemento.chave);
+
+        insere_esquerda(dequePrincipal, elemento);
+    }
+    if (menu == 3){
+        remove_esquerda(dequePrincipal, &elemento);
+    }
+    if (menu == 4){
+        remove_direita(dequePrincipal, &elemento);
+    }
+    if (menu == 5){
+        impressaoEsqDir(dequePrincipal);
+    }
+    if (menu == 6){
+        impressaoDirEsq(dequePrincipal);
+    }
+    if (menu == 7){
+        exit;
+    }
+    
+    return 1;
+
+}
