a73ce768b932dd3775bd51a7166421bfd2a2edcd
Olga Maria <olgamariasssantos@gmail.com>
2023-09-27 15:31:25 -0300

    Update ordenacao.rkt

--- ProgramacaoFuncional/Trabalho1/ordenacao.rkt
+++ ProgramacaoFuncional/Trabalho1/ordenacao.rkt
@@ -3,13 +3,12 @@
 
 (struct qtde_palavra (qtde pal)#:transparent)
 
-(define lista2 (list (qtde_palavra 2 "a") (qtde_palavra 3 "f") (qtde_palavra 3 "c") (qtde_palavra 2 "e") (qtde_palavra 2 "b") (qtde_palavra 1 "d")))
-(define lista-ordenada (list (qtde_palavra 3 "c") (qtde_palavra 3 "f") (qtde_palavra 2 "a") (qtde_palavra 2 "b") (qtde_palavra 2 "e") (qtde_palavra 1 "d")))
+(define lista2 (list (qtde_palavra 2 "a") (qtde_palavra 3 "f") (qtde_palavra 3 "c") (qtde_palavra 2 "e") (qtde_palavra 2 "b") (qtde_palavra 3 "d")))
 
 ;Recebe uma lista e retorna ela ordenada
 (define (ordena lista)
   (cond
-    [(empty? (rest lista)) (first lista)]
+    [(empty? (rest lista)) lista]
     [(> (qtde_palavra-qtde (first lista)) (qtde_palavra-qtde (first (rest lista))))
          (cons (first lista)
                (ordena (rest lista)))]
@@ -25,11 +24,19 @@
                 (ordena (cons (first lista)
                               (cdr (rest lista)))))]))
 
-(define (percorre-para-ordenar lista)
+;Vai percorrer a lista, verificando se os elementos estão na posição correta,
+;caso não estejam, chama função ordena, e verifica o retorno da ordena
+;no momento que verificar e todos os elementos estiverem corretos retorna a lista ordenada
+(define (verifica-lista-ordenada lista)
   (cond
     [(empty? lista) empty]
-    [else (ordena (lista)) ]))
-  
-(ordena lista2)
-;(examples
- ;(check-equal? (ordena lista2) (lista-ordenada)))
+    [(< (qtde_palavra-qtde (first lista)) (qtde_palavra-qtde (first (rest lista))))
+        (verifica-lista-ordenada (ordena lista))]
+    [(and (= (qtde_palavra-qtde (first lista)) (qtde_palavra-qtde (first (rest lista))))
+          (string>? (qtde_palavra-pal (first lista)) (qtde_palavra-pal (first (rest lista)))))
+          (verifica-lista-ordenada (ordena lista))]
+    [else lista]))
+
+;(verifica-lista-ordenada (rest (verifica-lista-ordenada (ordena lista))))
+   
+(verifica-lista-ordenada lista2)
