e49024adc560c9601c5b86eb38e20310ee14e71b
Olga Maria <olgamariasssantos@gmail.com>
2025-10-18 13:55:05 -0300

    lexico

---
+++ Compiladores/Analisadores/lexico.py
@@ -0,0 +1,73 @@
+import ply.lex as lex
+
+# dictionary of reserved words
+reserved = {
+    'program':  'PROGRAM',
+    'var':      'VAR',
+    'end':      'END',
+    'read':     'READ',
+    'write':    'WRITE',
+    'begin':    'BEGIN',
+    'integer':  'INT',
+    'boolean':  'BOOL',
+    'false':    'FALSE',
+    'true':     'TRUE',
+    'while':    'WHILE',
+    'do':       'DO',
+    'if':       'IF',
+    'then':     'THEN',
+    'else':     'ELSE',
+    'div':      'DIV',
+    'and':      'AND',
+    'or':       'OR',
+    'not':      'NOT'
+}
+
+literals = [':','=',';','(',')','<', '>','*','+','-',',','.']
+
+tokens = (
+   'NUM',
+   'ID',
+   'DIF',
+   'LESS',
+   'GREATER',
+   'ATRIB',
+) + tuple(reserved.values())
+
+t_DIF     = r'<>'
+t_LESS    = r'<='
+t_GREATER = r'>='
+t_ATRIB   = r':='
+
+# NUM: Sequency of one or more digits
+def t_NUM(t): # Os números negativos não serão processados na fase léxica, mas sim na fase sintática.
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# ID: start with a letter and optionally follows with a letter/digit/underscore
+def t_ID(t):
+    r'[A-Za-z][A-Za-z0-9_]*'
+    t.type = reserved.get(t.value, 'ID') # Check if the word is reserved
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    t.lexer.line_start = t.lexpos + len(t.value) # update line_start to count columns
+
+# Count columns in line
+def calculate_column(t, lexico):
+    line_start = getattr(lexico, 'line_start', 0)
+    return t.lexpos - line_start + 1
+
+# Ignore spaces and tabulations
+t_ignore = ' \t'
+
+# Report error and ignore invalid simbol
+def t_error(t):
+    col = calculate_column(t, t.lexer)
+    print(f"ERRO: Símbolo ilegal {t.value[0]!r} na linha {t.lineno}, coluna {col}")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
---
+++ Compiladores/Analisadores/main.py
@@ -0,0 +1,24 @@
+from sys import argv
+from lexico import lexer, calculate_column
+
+if len(argv) == 2:
+    with open(argv[1], 'r') as entrada:
+        data = entrada.read()
+        lexer.input(data)
+
+        while True:
+            tok = lexer.token() # return next token
+            if not tok:
+                break
+            col = calculate_column(tok, lexer)
+            print(f"<{tok.type}, {tok.value!r}> na linha: {tok.lineno}, coluna:{col}")
+else:
+    print('Necessário informar o arquivo .tascal')
+
+# aqui vai ter a conexão com o yacc (sintatico)
+# trabalhar na tabela de simbolos
+# Lembrando que alguns casoso lexico vai gerar para o sintatico dar o erro
+
+
+# PRA TABELA DE SIMBOLOS USAR DICT
+# 
